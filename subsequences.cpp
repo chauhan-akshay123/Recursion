dcfbb5f39724e80078b67935628dfce36d9d7b95
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2022-08-05 21:16:38 +0530

    Add files via upload

---
+++ subsequences.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+void solve(string ip,string op){
+    if(ip.length()==0){
+        cout<<op<<" ";
+        return; 
+    }
+    string op1 = op;
+    string op2 = op;
+    op2.push_back(ip[0]);
+    ip.erase(ip.begin()+0);
+    solve(ip,op1);
+    solve(ip,op2);
+}
+
+int main(){
+string ip;
+cout<<"Enter your string : "<<endl;
+cin>>ip;
+string  op ="";
+solve(ip,op);
+
+    return 0;
+}
